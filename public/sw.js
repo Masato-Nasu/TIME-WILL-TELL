12e2838031ab4eefae872618d4c099b1284f2546
那須　雅人 <1bitexist@gmail.com>
2026-08-08 23:02:42 +0900

    Do not cache private message routes

--- public/sw.js
+++ public/sw.js
@@ -4,7 +4,7 @@ self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', e => {
   const u = new URL(e.request.url);
-  if (e.request.method !== 'GET' || u.pathname.startsWith('/api/') || u.pathname.startsWith('/line/')) return;
+  if (e.request.method !== 'GET' || u.pathname.startsWith('/api/') || u.pathname.startsWith('/line/') || u.pathname.startsWith('/gmail/') || u.pathname.startsWith('/m/')) return;
   e.respondWith(fetch(e.request).then(r => {
     const copy = r.clone();
     caches.open(CACHE).then(c => c.put(e.request, copy));
