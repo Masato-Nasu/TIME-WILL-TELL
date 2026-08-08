7b04c78ba0384775d960f59b9d18e3a6f99dd947
那須　雅人 <1bitexist@gmail.com>
2026-08-08 22:24:53 +0900

    Add service worker v0.1.7

---
+++ public/sw.js
@@ -0,0 +1,13 @@
+const CACHE = 'time-will-tell-v0.1.7';
+const STATIC = ['/', '/styles.css', '/app.js', '/icon.svg', '/manifest.webmanifest'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(STATIC)).then(() => self.skipWaiting())));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', e => {
+  const u = new URL(e.request.url);
+  if (e.request.method !== 'GET' || u.pathname.startsWith('/api/') || u.pathname.startsWith('/line/')) return;
+  e.respondWith(fetch(e.request).then(r => {
+    const copy = r.clone();
+    caches.open(CACHE).then(c => c.put(e.request, copy));
+    return r;
+  }).catch(() => caches.match(e.request)));
+});
