c9dd91e26d868f6eec81cb5d59b08ee7dd7bbffd
那須　雅人 <1bitexist@gmail.com>
2026-08-08 23:44:49 +0900

    v0.1.10: bump PWA cache and bypass message routes

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'time-will-tell-v0.1.9';
+const CACHE = 'time-will-tell-v0.1.10';
 const STATIC = ['/', '/styles.css', '/app.js', '/icon.svg', '/manifest.webmanifest'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(STATIC)).then(() => self.skipWaiting())));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
